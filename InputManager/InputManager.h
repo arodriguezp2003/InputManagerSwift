0b4f01ebf5629825c7917d88af50312991682ec4
Alejandro Rodriguez <alejandrorodriguezpena@gmail.com>
2018-12-11 11:34:00 -0300

    Initial Commit

---
+++ InputManager/InputManager.h
@@ -0,0 +1,19 @@
+//
+//  InputManager.h
+//  InputManager
+//
+//  Created by Alejandro  Rodriguez on 12/11/18.
+//  Copyright © 2018 Alejandro  Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InputManager.
+FOUNDATION_EXPORT double InputManagerVersionNumber;
+
+//! Project version string for InputManager.
+FOUNDATION_EXPORT const unsigned char InputManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InputManager/PublicHeader.h>
+
+
---
+++ InputManagerTests/InputManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  InputManagerTests.swift
+//  InputManagerTests
+//
+//  Created by Alejandro  Rodriguez on 12/11/18.
+//  Copyright © 2018 Alejandro  Rodriguez. All rights reserved.
+//
+
+import XCTest
+@testable import InputManager
+
+class InputManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
